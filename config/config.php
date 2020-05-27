25340310ac0e792d72a1b8995462b3cfa673c773
griff9214 <griff@list.ru>
2020-05-27 17:06:23 +0300

    Configuring App by autoloading config files

--- config/config.php
+++ config/config.php
@@ -1,6 +1,7 @@
 <?php
 
-return array_merge_recursive(
-    require_once "config/autoload/auth.global.php",
-    require_once "config/autoload/app.global.php",
-);
+$configs = array_map(function ($el) {
+    return require $el;
+}, glob(__DIR__ . '/autoload/{{,*.}global,{,*.}local}.php', GLOB_BRACE));
+
+return array_merge_recursive(...$configs);
