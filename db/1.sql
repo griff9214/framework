29b5f9aadce237cf698011a3f80897d441dfdeec
griff9214 <griff@list.ru>
2020-07-27 17:23:09 +0300

    sqlite starting

--- db/1.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    email
-FROM
-    users
-GROUP BY
-    email
-Having
-        COUNT(email) > 1
--- db/3.sql
+++
@@ -1,7 +0,0 @@
-SELECT login
-FROM users
-WHERE id IN (
-    SELECT user_id
-    FROM orders
-    GROUP BY user_id
-    Having COUNT(user_id) > 2)
